7334db512c9171d3f6847b7a5f2e877258268838
Rimvydas Kulda <rimvydas.kulda@gmail.com>
2018-07-16 00:09:51 +0300

    added password generation

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const passwordGenerator = require("password-generator");
 
 const app = express();
 app.listen(process.env.PORT || 5000);
 
 // Server index page
 app.get("/", (req, res) => {
-  res.send("Deployed!");
+  const password = passwordGenerator(16, false);
+  res.send(password);
 });
